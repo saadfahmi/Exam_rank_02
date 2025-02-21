72aca0a66c9d077e20f09048dd4aa4862c4ad4e2
saadfahmitest <s.fahmi2618@uca.ac.ma>
2025-02-21 17:08:30 +0100

    subject files

---
+++ 01_union/union.c
@@ -0,0 +1,42 @@
+#include <unistd.h>
+
+void	check_ascii(char *s, int *ascii)
+{
+	int		idx = 0;
+
+	while (s[idx])
+	{
+		ascii[(int)s[idx]] = 1;
+		idx++;
+	}
+}
+
+void	print_parameter(char *s, int *ascii)
+{
+	int		idx = 0;
+
+	while (s[idx])
+	{
+		if (ascii[(int)s[idx]] == 1)
+		{
+			write(1, (s + idx), 1);
+			ascii[(int)s[idx]] = 0;
+		}
+		idx++;
+	}
+}
+
+int		main(int argc, char *argv[])
+{
+	int		ascii[255] = {0, };
+
+	if (argc == 3)
+	{
+		check_ascii(argv[1], ascii);
+		check_ascii(argv[2], ascii);
+		print_parameter(argv[1], ascii);
+		print_parameter(argv[2], ascii);
+	}
+	write(1, "\n", 1);
+	return (0);
+}
